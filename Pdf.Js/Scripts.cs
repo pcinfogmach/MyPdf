5966ae1a23a01a8ee7895ae58f02c699f40dfcf7
pcinfogmach <pcinfoGmach@gmail.com>
2024-11-12 00:40:25 +0200

    a

--- Pdf.Js/Scripts.cs
+++ Pdf.Js/Scripts.cs
@@ -5,8 +5,7 @@ namespace Pdf.Js
     {
         public static string EditButtonsScript()
         {
-            return @"
-const secondaryToolbar = document.getElementById('secondaryToolbarButtonContainer');
+            return @"const secondaryToolbar = document.getElementById('secondaryToolbarButtonContainer');
 const openFileButton = document.getElementById('secondaryOpenFile');
 const saveButton = document.getElementById('downloadButton');
 const saveButtonParent = saveButton?.parentElement;
@@ -15,17 +14,13 @@ if (openFileButton) {
    // Clone and replace the original open file button
    const newOpenFileButton = openFileButton.cloneNode(true);
    openFileButton.remove();
-   newOpenFileButton.className = ""toolbarButton"";
+   newOpenFileButton.className = ""toolbarButton""; 
    saveButtonParent?.appendChild(newOpenFileButton);
    
    // Add event listener for OpenFile action
    newOpenFileButton.addEventListener('click', function () {
-      window.chrome.webview.postMessage({ action: ""OpenFile"" });
+      window.chrome.webview.postMessage({ action: ""OpenFile"" }); 
    });
-
-   const newScreenCaptureButton = newOpenFileButton.cloneNode(true);
-   newScreenCaptureButton.textContent = ""⬚"";
-   saveButtonParent?.insertBefore(newScreenCaptureButton, saveButtonParent.firstChild);
 }
 
 if (saveButton && secondaryToolbar) {
@@ -35,22 +30,23 @@ if (saveButton && secondaryToolbar) {
    // Check for Hebrew text to set button label and title accordingly
    if (newSaveAsButton.textContent?.startsWith('ש')) {
       newSaveAsButton.textContent = ""שמור בשם""; // Hebrew text
-      newSaveAsButton.title = ""שמור בשם"";
+      newSaveAsButton.title = ""שמור בשם""; 
    } else {
       newSaveAsButton.textContent = ""Save As""; // English text
-      newSaveAsButton.title = ""Save As"";
+      newSaveAsButton.title = ""Save As""; 
    }
 
-   newSaveAsButton.className = ""toolbarButton labeled"";
+   newSaveAsButton.className = ""toolbarButton labeled""; 
 
    // Insert newSaveAsButton as the first child of secondaryToolbar
    secondaryToolbar.insertBefore(newSaveAsButton, secondaryToolbar.firstChild);
 
    // Add event listener for SaveAs action
    newSaveAsButton.addEventListener('click', function () {
-      window.chrome.webview.postMessage({ action: ""SaveAs"" });
+      window.chrome.webview.postMessage({ action: ""SaveAs"" }); 
    });
 }
+
 
 ";
         }
